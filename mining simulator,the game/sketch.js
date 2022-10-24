6d8f86b651387eb2e4351dd71f0d2a208b96b1b9
GabieTheCodingLad <89045961+GabieTheCodingLad@users.noreply.github.com>
2022-10-24 18:33:44 -0300

    Update sketch.js

--- mining simulator,the game/sketch.js
+++ mining simulator,the game/sketch.js
@@ -1,25 +1,48 @@
-var Ores = ['iron','copper','gold']
+var Ores = ['Iron','Copper','Gold','Lead','Platinum','Aluminium','Bismuth','Australium','Magma','Void']
 var Oring = 0
 var Ored = 0
-
+var sequence = 20
 
 function preload() {
   
 }
 
 function setup() {
-    createCanvas(1325,600);
-Orenumbr = round(random(1,30))
-Oring = round(random(0,Ores.length))
+    
+    createCanvas(1325,650);
+    background(200)
+Orenumbr = round(random(1,15))
+Oring = round(random(0,Ores.length - 1))
 Ored = Ores[Oring]
-console.log(Oring)
+for(var i = 0 ; i<sequence ; i++){
+        
+    Orenumbr = round(random(1,15))
+    Oring = round(random(0,Ores.length - 1))
+    Ored = Ores[Oring]
+    text("You found "+ Orenumbr + " " + Ored,633,30 + i*20) 
+    
+    }
 }
 
-function draw() {
-background(220)
-//"You found" + Round(random(1,30)) + Ores[Oring]
-text("You found "+ Orenumbr + " " + Ored,300,300)
+function draw() {   
+if (mouseIsPressed){
+    Randomize()
 }
+}
+
+function Randomize(){
+    background(200)
+    for(var i = 0 ; i<sequence ; i++){
+        
+        Orenumbr = round(random(1,15))
+        Oring = round(random(0,Ores.length - 1))
+        Ored = Ores[Oring]
+        text("You found "+ Orenumbr + " " + Ored,633,30 + i*20) 
+        
+        }
+        
+        
+}  
 
 
 
